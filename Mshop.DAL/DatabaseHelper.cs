ef33067689093158199ae59837399afda5c75ae9
lx <1liu2xiang1@sina.cn>
2017-03-30 22:17:10 +0800

    添加项目

---
+++ Mshop.DAL/DatabaseHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace Mshop.DAL
+{
+    public class DatabaseHelper
+    {
+        private string connectionStr;
+
+        public DatabaseHelper()
+        {
+            connectionStr = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+        }
+
+        public object GetAll()
+        {
+            using (var con = new MySqlConnection(connectionStr))
+            {
+                con.Open();
+                var commandText = "select name from abpusers where id=@id";
+                var command = new MySqlCommand(commandText, con);
+                command.CommandType = System.Data.CommandType.Text;
+                command.Parameters.Add(new MySqlParameter("@id",1));
+                object result = command.ExecuteScalar();
+                return result;
+            }
+        }
+    }
+}
---
+++ Mshop.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using Mshop.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mshop.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new DatabaseHelper().GetAll();
+        }
+    }
+}
